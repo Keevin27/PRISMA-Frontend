0efc452a69868859f4866a61ffef6f8e3087210a
KevinCM07 <kcuellarmejia@gmail.com>
2025-06-16 01:34:37 -0600

    Menu de hamburguesa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./PaqueteEscolar/paquete-escolar/paquete-escolar.component').then((m) => m.PaqueteEscolarComponent),
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.component').then(m => m.HomeComponent),
+      },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+];
---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css'],
+  imports: [SidebarComponent, RouterOutlet],
+})
+export class LayoutComponent {}
