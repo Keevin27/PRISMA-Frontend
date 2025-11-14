5746d1e9bee358ec51ef9bbac7499611597aba26
Arce <113496236+Arce-dn@users.noreply.github.com>
2025-11-13 18:24:23 -0600

    Implementación restablecer de contraseña

--- src/app/Auth/Login/login.component.ts
+++ src/app/Auth/Login/login.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -20,7 +21,7 @@ export class LoginComponent {
 
   error = '';
 
-  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {}
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router, private http: HttpClient) {}
 
   onSubmit() {
     if (this.form.invalid) return;
@@ -34,5 +35,22 @@ export class LoginComponent {
         this.error = 'Correo o contraseña inválidos';
       },
     });
+  }
+
+  recuperar() {
+    const correo = this.form.get('correo')?.value;
+    if (!correo) {
+      this.error = "Debe ingresar su correo para recuperar la contraseña";
+      return;
+    }
+
+    this.http.post('http://localhost:8080/api/usuarios/recuperar', { correo })
+      .subscribe({
+        next: () => alert("Se envió un correo con la nueva contraseña"),
+        error: err => {
+          console.error(err);
+          this.error = err.error?.message || err.message || "Error al recuperar contraseña";
+        }
+      });
   }
 }
